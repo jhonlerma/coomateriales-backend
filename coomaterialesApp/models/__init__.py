d895f84b97d50835bc124516af3768d0bf8f9666
jhonlerma <john.enfermeria3645@gmail.com>
2022-08-03 00:33:32 -0500

    me trigo los cambios de jimmy al main y corrijo dependencias sobrantes

--- coomaterialesApp/models/__init__.py
+++ coomaterialesApp/models/__init__.py
@@ -1,7 +1,5 @@
 from .user import User
 from .categoria import Categoria
 from .fabricante import Fabricante
-from .familiacatalogo import FamiliaCatalogo
 from .producto import Producto
 from .proveedor import Proveedor
-
