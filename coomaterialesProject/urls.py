b75ffe111cd8d7851b2c284aa4b8218a74dc81ff
jhonlerma <john.enfermeria3645@gmail.com>
2022-08-04 14:33:38 -0500

    se crea vista proveedor vista borrar 2

--- coomaterialesProject/urls.py
+++ coomaterialesProject/urls.py
@@ -29,35 +29,35 @@ urlpatterns = [
     path('user/create/', views.UserCreateView.as_view()),
     path('user/read/<int:pk>/', views.UserDetailView.as_view()),
     path('user/update/',views.UserEditView.as_view()),
-    path('user/delete/', views.UserDeleteView.as_view()),
+    path('user/delete/<int:pk>/', views.UserDeleteView.as_view()),
     path('user/list/',views.UserListView.as_view()),
 
     # CATEGORIA PATHS
     path('categoria/create/', views.CategoriaCreateView.as_view()),
     path('categoria/read/<int:pk>/', views.CategoriaDetailView.as_view()),
     path('categoria/update/',views.CategoriaEditView.as_view()),
-    path('categoria/delete/', views.CategoriaDeleteView.as_view()),
+    path('categoria/delete/<int:pk>/', views.CategoriaDeleteView.as_view()),
     path('categoria/list/',views.CategoriaListView.as_view()),
 
     # FABRICANTE PATHS
     path('fabricante/create/', views.FabricanteCreateView.as_view()),
     path('fabricante/read/<int:pk>/', views.FabricanteDetailView.as_view()),
     path('fabricante/update/',views.FabricanteEditView.as_view()),
-    path('fabricante/delete/', views.FabricanteDeleteView.as_view()),
+    path('fabricante/delete/<int:pk>/', views.FabricanteDeleteView.as_view()),
     path('fabricante/list/',views.FabricanteListView.as_view()),
 
     # PROVEEDOR PATHS
     path('proveedor/create/', views.ProveedorCreateView.as_view()),
     path('proveedor/read/<int:pk>/', views.ProveedorDetailView.as_view()),
     path('proveedor/update/',views.ProveedorEditView.as_view()),
-    path('proveedor/delete/', views.ProveedorDeleteView.as_view()),
+    path('proveedor/delete/<int:pk>/', views.ProveedorDeleteView.as_view()),
     path('proveedor/list/',views.ProveedorListView.as_view()),
 
     # PRODUCTO PATHS
     path('producto/create/', views.ProductoCreateView.as_view()),
     path('producto/read/<int:pk>/', views.ProductoDetailView.as_view()),
     path('producto/update/',views.ProductoEditView.as_view()),
-    path('producto/delete/', views.ProductoDeleteView.as_view()),
+    path('producto/delete/<int:pk>/', views.ProductoDeleteView.as_view()),
     path('producto/list/',views.ProductoListView.as_view()),
 
 ]
