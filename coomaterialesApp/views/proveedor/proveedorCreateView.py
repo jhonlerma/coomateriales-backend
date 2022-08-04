0bab4b512d66a31ff640ac1fb1aa758901b7e1a0
jhonlerma <john.enfermeria3645@gmail.com>
2022-08-04 08:34:06 -0500

    inician pruebas de vistas

--- coomaterialesApp/views/proveedor/proveedorCreateView.py
+++ coomaterialesApp/views/proveedor/proveedorCreateView.py
@@ -9,5 +9,19 @@ from coomaterialesApp.serializers.proveedorserializer import ProveedorSerializer
 class ProveedorCreateView(views.APIView):
     #List del usuario por metodo get
     def post(self,request,*args,** kwargs):
-        # codigo aqui borrar el pass
-        pass
+        serializer = ProveedorSerializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        #acceso al request.data
+        userData = {
+            "id": request.data["id"],
+            "nit_proveedor": request.data["nit_proveedor"],
+            "nombre_proveedor": request.data["nombre_proveedor"],
+            "telefono_proveedor": request.data["telefono_proveedor"],
+            "correo_proveedor": request.data["correo_proveedor"],
+            "direccion_proveedor": request.data["direccion_proveedor"], 
+        }
+
+        #envío de la respuesta con validación y el status de creación 201  
+        return Response (userData, status= status.HTTP_201_CREATED)
--- coomaterialesApp/views/proveedor/proveedorDetailView.py
+++ coomaterialesApp/views/proveedor/proveedorDetailView.py
@@ -1,13 +1,19 @@
+from django.conf import settings
 from rest_framework import status,views
 from rest_framework.response import Response #Retornanr la respuesta al usuario
 from rest_framework_simplejwt.backends import TokenBackend
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer #serializador que se encarga apartir del usuario y contraseña genera los token y los asocian 
 from coomaterialesApp.serializers.proveedorserializer import ProveedorSerializer #serializador de user
+from coomaterialesApp.models.proveedor import Proveedor
 
 
 class ProveedorDetailView(views.APIView):
+    queryset = Proveedor.objects.all()
+    serializer_class = ProveedorSerializer
+    permission_classes = [IsAuthenticated]
+
     #List del usuario por metodo get
     def get(self,request,*args,** kwargs):
-        # codigo aqui borrar el pass
-        pass
+
+        return super().get(request, *args, **kwargs)
