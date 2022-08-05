d0de35eb8752ab25ae31f5350d502f26cfca5550
jhonlerma <john.enfermeria3645@gmail.com>
2022-08-05 10:23:14 -0500

    otro mas

--- coomaterialesApp/serializers/productoserializer.py
+++ coomaterialesApp/serializers/productoserializer.py
@@ -1,10 +1,14 @@
 from coomaterialesApp.models.producto import Producto
 from coomaterialesApp.models.categoria import Categoria
 from coomaterialesApp.serializers.categoriaserializer import CategoriaSerializer
+from coomaterialesApp.serializers.fabricanteserializer import FabricanteSerializer
+from coomaterialesApp.serializers.proveedorserializer import ProveedorSerializer
 from rest_framework import serializers
 
 class ProductoSerializer(serializers.ModelSerializer):
     categoria = CategoriaSerializer()
+    fabricante = FabricanteSerializer()
+    proveedor = ProveedorSerializer()
     class Meta:
         model = Producto
         fields = [
@@ -14,9 +18,9 @@ class ProductoSerializer(serializers.ModelSerializer):
             'precio_unit_producto',
             'resumen_producto',
             'detalle_producto',
-            'categoria_producto',
-            'fabricante_producto',
-            'proveedor_producto'
+            'categoria',
+            'fabricante',
+            'proveedor'
         ]
 
 
